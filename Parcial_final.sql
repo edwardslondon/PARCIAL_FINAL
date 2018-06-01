83f16ae0d98c3813863c103d35b64a2da5f8e915
Edward Londoño <edwards.lonavi@gmail.com>
2018-06-01 07:56:55 -0500

    commit store procedure

--- Parcial_final.sql
+++ Parcial_final.sql
@@ -34,3 +34,31 @@ begin
   numbers := number_array(160, 3, 1719, 19, 11, 13, -21);
   dbms_output.put_line(FIND_OUTLIER(numbers));
 end;
+
+
+
+CREATE OR REPLACE PROCEDURE CALCULAR_SIMULACION(
+    tasa_efectiva_anual IN DECIMAL,
+    numero_coutas IN DECIMAL,
+    valor_prestamo IN DECIMAL,
+    cuota_mensual IN DECIMAL
+) AS
+    tasa_nominal_mensual DECIMAL(15,4);
+    tasa DECIMAL(15,4);
+    BEGIN
+        DELETE FROM amortizacion;
+        tasa_nominal_mensual := power ( (1 + (tasa_efectiva_anual / 100)), (30 / 360)) - 1;
+        tasa := (tasa_nominal_mensual / 100);
+        cuota_mensual = ()
+        DBMS_OUTPUT.PUT_LINE (tasa_nominal_mensual);
+END;
+
+DECLARE 
+    tasa_anual_porcentaje DECIMAL(15,4) := 29.8407;
+    total_coutas DECIMAL(15,2) := 72.0;
+    valor_prestamo DECIMAL(15,2) := 6000000;
+BEGIN
+ CALCULAR_SIMULACION(tasa_anual_porcentaje,total_coutas,valor_prestamo);
+END;
+    
+    
